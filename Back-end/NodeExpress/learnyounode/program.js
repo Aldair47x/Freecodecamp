b6239a7e2d995c0688311625badc59f7b6b202e7
Aldair47x <aldair47x@gmail.com>
2018-01-02 00:03:51 -0500

    In process the learnyounode challenge.

---
+++ Back-end/NodeExpress/learnyounode/program.js
@@ -0,0 +1,15 @@
+//Hello world
+/*
+console.log("Hello world!");
+*/
+
+//Baby steps
+/*
+let result = 0
+
+for (let i = 2; i < process.argv.length; i++) {
+  result += Number(process.argv[i])
+}
+
+console.log(result);
+*/
